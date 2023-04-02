9e363f4a0a8e08cce04a26c49d0ef5091455f6ba
Lookatthispieceofpaper <113527910+Lookatthispieceofpaper@users.noreply.github.com>
2023-04-02 01:00:43 +0000

    ui commit! converted from figma to react.

---
+++ Coding screen in react format/src/App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Group3 } from './components/Group3/Group3';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.storybrainResets} ${classes.root}`}>
+      <Group3 />
+    </div>
+  );
+});
