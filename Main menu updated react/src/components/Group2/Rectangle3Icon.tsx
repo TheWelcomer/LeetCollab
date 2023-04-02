e75bd1250f009742502d970b1383793b9af5aefa
mjuarez333 <115038931+mjuarez333@users.noreply.github.com>
2023-04-02 07:15:03 -0400

    Delete Main menu updated react directory

--- Main menu updated react/src/components/Group2/Rectangle3Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Rectangle3Icon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 405 93' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path d='M0 0H405V93H0V0Z' fill='white' />
-  </svg>
-);
-const Memo = memo(Rectangle3Icon);
-export { Memo as Rectangle3Icon };
